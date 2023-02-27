dfab3d05d24cfac718c8714da627e7d64c7cb64f
Maximiliano Minetto <maximinetto@gmail.com>
2023-02-26 23:29:23 -0300

    add register handler

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { loginHandler, profileHandler } from "../controller/auth.controller";
-import requireAuth from "../middleware/requireAuth";
+import {
+  loginHandler,
+  profileHandler,
+  registerHandler,
+} from "../controllers/auth.controller";
+import requireAuth from "../middlewares/requireAuth";
 
 const router = Router();
 
 router.post("/login", loginHandler);
 router.get("/profile", requireAuth, profileHandler);
+router.post("/register", registerHandler);
 
 export default router;
