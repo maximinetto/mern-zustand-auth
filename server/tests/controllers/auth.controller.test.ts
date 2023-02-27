ebd5bfbbd6273f42a1dcab6e7d1c030eb27ccc59
Maximiliano Minetto <maximinetto@gmail.com>
2023-02-26 23:28:35 -0300

    add tests

--- server/tests/controllers/auth.controller.test.ts
+++ server/tests/controllers/auth.controller.test.ts
@@ -1,19 +1,25 @@
 import request from "supertest";
 import app from "../../app";
+import { User } from "../../models";
+import assertDefined from "../utils/assertDefined";
 
 async function getToken(): Promise<{
   status: number;
   body: { token: string };
+  userInfo: {
+    email: string;
+    password: string;
+  };
 }> {
-  const response = await request(app).post("/login").send({
-    email: "maximinetto@hello.com",
-    password: "12345678",
-  });
+  const userInfo = { email: "maximinetto@hello.com", password: "12345678" };
+
+  const response = await request(app).post("/login").send(userInfo);
 
   const { status, body } = response;
   return {
     status,
     body,
+    userInfo,
   };
 }
 
@@ -24,6 +30,35 @@ describe("Auth controller", () => {
     expect(status).toBe(200);
     expect(body).toHaveProperty(["token"]);
     expect(body.token).toBeTypeOf("string");
+  });
+
+  it("should create a new user with a successful message", async () => {
+    const userInfo = { email: "maximinetto@hello.com", password: "12345678" };
+    const response = await request(app).post("/register").send(userInfo);
+    const { body, status } = response;
+    const user = await User.findOne({ email: userInfo.email });
+
+    expect(status).toBe(201);
+    expect(body).toEqual({ message: "User registered" });
+    assertDefined(user);
+    expect(user.email).toBe(userInfo.email);
+  });
+
+  it("should not register a user with a duplicate email", async () => {
+    const userInfo = { email: "maximinetto@hello.com", password: "12345678" };
+    await User.create({ ...userInfo, password: "1234" });
+    const response = await request(app).post("/register").send(userInfo);
+    const { body, status } = response;
+    const user = await User.findOne({ email: userInfo.email });
+
+    expect(status).toBe(409);
+    expect(body).toEqual({
+      message: "User already exists",
+    });
+
+    assertDefined(user);
+    expect(user.email).toBe(userInfo.email);
+    expect(user.password).not.toBe(userInfo.password);
   });
 
   it("should not get the profile and return a 401 status code", async () => {
