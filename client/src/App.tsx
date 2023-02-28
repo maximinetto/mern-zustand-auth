ee09174f53170b1a152435bab75eca6e459a6d51
Maximiliano Minetto <maximinetto@gmail.com>
2023-02-28 11:54:39 -0300

    Add session validation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,26 +2,29 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navigation from "./components/Navigation";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Session from "./components/Session";
+import useIsLogged from "./hooks/useIsLogged";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import RegisterPage from "./pages/RegisterPage";
-import { useAuthStore } from "./store/auth";
 
-export default function App() {
-  const isLogged = useAuthStore((state) => state.isLogged);
+export default function App(): JSX.Element {
+  const isLogged = useIsLogged();
 
   return (
     <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route element={<ProtectedRoute isAllowed={isLogged} />}>
-          <Route path="/profile" element={<ProfilePage />} />
-        </Route>
-      </Routes>
+      <Session>
+        <Navigation />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route element={<ProtectedRoute isAllowed={isLogged} />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
+        </Routes>
+      </Session>
     </BrowserRouter>
   );
 }
