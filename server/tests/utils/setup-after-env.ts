2b47eadd92239b3dea2ee5272367a39fdf2b8986
Maximiliano Minetto <maximinetto@gmail.com>
2023-02-28 11:49:57 -0300

    Remove log

--- server/tests/utils/setup-after-env.ts
+++ server/tests/utils/setup-after-env.ts
@@ -3,7 +3,6 @@ import { HOST_DB, PASS_DB, PORT_DB, USER_DB } from "../../config";
 
 beforeAll(async () => {
   mongoose.set("strictQuery", false);
-  console.debug(USER_DB, PASS_DB, HOST_DB, PORT_DB);
   await mongoose.connect(`mongodb://${HOST_DB}:${PORT_DB}/auth`, {
     authSource: "admin",
     user: USER_DB,
