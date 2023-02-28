e05b7966c61a12a7d9de421393ee20212042c0a5
Maximiliano Minetto <maximinetto@gmail.com>
2023-02-28 15:31:14 -0300

    Add port env

--- server/config.ts
+++ server/config.ts
@@ -6,3 +6,4 @@ export const USER_DB = process.env.USER_DB ?? "root";
 export const PASS_DB = process.env.PASS_DB ?? "1234";
 export const HOST_DB = process.env.HOST_DB ?? "localhost";
 export const PORT_DB = process.env.PORT_DB ?? "27017";
+export const PORT = process.env.PORT ?? "3000";
