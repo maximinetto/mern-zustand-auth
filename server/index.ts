3e13da8dd74ac6cdec9fbd21cc9132e72c939eec
Maximiliano Minetto <maximinetto@gmail.com>
2023-02-28 11:57:01 -0300

    add mongodb connection and close resources

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,26 @@
+import { type IncomingMessage, type Server, type ServerResponse } from "http";
+import mongoose from "mongoose";
 import app from "./app";
+import connect from "./db/connect";
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-});
+let server: Server<typeof IncomingMessage, typeof ServerResponse>;
+
+connect()
+  .then(() => {
+    const PORT = 3000;
+    server = app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Mongo err: ", { err });
+  });
+
+const closeServer = (): void => {
+  void mongoose.disconnect();
+  server.close();
+};
+
+process.on("SIGINT", closeServer);
+
+process.on("SIGTERM", closeServer);
